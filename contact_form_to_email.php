7e9ad6870c3fca1b5df48a74c73e1bb5aa47a8cf
Julie Belleville <35176177+jbellevi@users.noreply.github.com>
2018-07-11 21:00:09 -0700

    website with form

---
+++ contact_form_to_email.php
@@ -0,0 +1,93 @@
+<?php
+
+	if (!isset($_POST['SUBMIT']))
+	{
+		//if form not submitted
+		echo "error: form not submitted";
+	}
+
+	$name = $_POST['name'];
+	$contact = $_POST['contact'];
+	$message = $_POST['message'];
+
+	//check if necessary values have been entered
+	if (empty($name) || empty($contact) || empty($message))
+	{
+		echo "Name, contact information and message mandatory";
+		exit;
+	}
+
+	if (isInjected($contact) || isInjected($name) || isInjected($message))
+	{
+			echo "Potentially dangerous content values";
+			exit;
+	}
+
+	$email_from = 'juliebelleville0@gmail.com';
+	$email_subject = "Form from Personal Website";
+	$email_body = "New message from: $name. Contact info: $contact. \n Message: \n $message"
+
+	$email_to = 'julie.belleville@telus.net';
+	$headers = "From: $email_from";
+
+	//send email
+	mail($email_to, $email_subject, $email_body, $headers);
+
+	echo "form submitted: thank you";
+
+	function IsInjected($str)
+	{
+ 		 $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+	 	 $inject = join('|', $injections);
+	 	 $inject = "/$inject/i";
+	  	if(preg_match($inject,$str))
+	    {
+	    	return true;
+	  	}
+	  	else
+	    {
+	    	return false;
+	  	}
+	}
+   
+?> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
